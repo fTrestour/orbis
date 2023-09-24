da2ec8822bc9cb9a4f04314b3d2035b86e5ae6fd
Fabien <ftrestour@gmail.com>
2023-09-24 22:00:33 +0200

    ♻️ Use parse_file_as_program

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
-use std::{path::Path, sync::Arc};
-use swc::config::IsModule;
-use swc_common::{
-    errors::{ColorConfig, Handler},
-    Globals, SourceMap, GLOBALS,
-};
-use swc_ecma_ast::{EsVersion, Program};
-use swc_ecma_parser::{Syntax, TsConfig};
+use std::path::Path;
+use swc_common::SourceMap;
+use swc_ecma_ast::{EsVersion, Module};
+use swc_ecma_parser::{parse_file_as_module, Syntax};
 
 pub fn get_imports(path: &Path) -> Vec<String> {
-    get_program(path)
-        .module()
-        .expect("Expected module, got script")
+    get_module(path)
         .body
         .into_iter()
         .filter_map(|module_item| {
@@ -22,30 +16,24 @@ pub fn get_imports(path: &Path) -> Vec<String> {
         .collect()
 }
 
-fn get_program(path: &Path) -> Program {
-    let code_map = Arc::<SourceMap>::default();
-    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, true, Some(code_map.clone()));
-    let compiler = swc::Compiler::new(code_map.clone());
+fn get_module(path: &Path) -> Module {
+    let code_map = SourceMap::default();
     let file = code_map.load_file(path).expect("Failed to load file");
-    let globals = Globals::default();
 
-    let program = GLOBALS.set(&globals, || {
-        compiler
-            .parse_js(
-                file,
-                &handler,
-                EsVersion::latest(),
-                Syntax::Typescript(TsConfig {
-                    tsx: true,
-                    decorators: false,
-                    dts: false,
-                    no_early_errors: true,
-                    disallow_ambiguous_jsx_like: true,
-                }),
-                IsModule::Bool(true),
-                None,
-            )
-            .expect("Failed to parse file")
-    });
+    let mut recovered_errors = vec![];
+    let program = parse_file_as_module(
+        &file,
+        Syntax::Typescript(Default::default()),
+        EsVersion::latest(),
+        None,
+        &mut recovered_errors,
+    )
+    .expect("Failed to parse file");
+
+    if !recovered_errors.is_empty() {
+        println!("{:?}", recovered_errors);
+        panic!("Failed to parse file");
+    };
+
     program
 }
