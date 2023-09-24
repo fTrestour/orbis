603a671b4127b1aeae6e7eb80b55474e5e69a15b
Fabien <ftrestour@gmail.com>
2023-09-24 22:21:01 +0200

    ✨ Handle folder

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,34 @@ use orbis::get_imports;
 #[derive(Parser, Debug)]
 struct Args {
     #[arg()]
-    file: String,
+    path: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let path = Path::new(&args.file);
-    let imports = get_imports(path);
+    show_path_imports(Path::new(&args.path))
+}
+
+fn show_path_imports(path: &Path) {
+    if path.is_file() {
+        let imports = get_imports(path);
+
+        show_dependencies(path, &imports);
+    } else if path.is_dir() {
+        for file in path.read_dir().expect("Failed to read directory") {
+            if let Ok(file) = file {
+                show_path_imports(&file.path())
+            }
+        }
+    } else {
+        panic!("Unexpected file type")
+    }
+}
+
+fn show_dependencies(path: &Path, imports: &Vec<String>) {
+    println!("{}", path.display());
     for import in imports {
-        println!("{}", import);
+        println!("  {}", import);
     }
 }
