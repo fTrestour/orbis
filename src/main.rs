c95b168efdc9ab0d444ba0f13c66b01c90570b17
Fabien <ftrestour@gmail.com>
2023-09-29 23:06:09 +0200

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use clap::Parser;
 use orbis::{get_imports, resolver::Resolver};
@@ -10,29 +10,32 @@ struct Args {
 
     #[clap(short = 'c', long = "config")]
     ts_config: Option<String>,
+
+    #[clap(short = 'p', long = "project")]
+    project_dir: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    if let Some(ts_config) = args.ts_config {
-        // TODO: Handle no ts
+    let resolver = if let Some(ts_config) = args.ts_config {
         let ts_config = Path::new(&ts_config);
-        let resolver = Resolver::try_from(ts_config).expect("plop");
+        Resolver::try_from(ts_config).expect("plop")
+    } else if let Some(project_dir) = args.project_dir {
+        Resolver::from_project_url(Path::new(&project_dir))
+    } else {
+        panic!("--project should be specified if --config is not");
+    };
 
-        let path = Path::new(&args.path);
-        if path.is_file() {
-            let imports = get_imports(path, &resolver).expect("yolo");
-            show_dependencies(path, imports);
-        } else {
-            panic!("Unexpected file type")
+    let path = Path::new(&args.path);
+    if path.is_file() {
+        let imports = get_imports(path, &resolver).unwrap();
+
+        println!("{}", path.display());
+        for import in imports {
+            println!("  {}", import.display());
         }
-    }
-}
-
-fn show_dependencies(path: &Path, imports: Vec<PathBuf>) {
-    println!("{}", path.display());
-    for import in imports {
-        println!("  {}", import.display());
+    } else {
+        panic!("This tool does not support directories");
     }
 }
